e254a7bd3f6f25a56d404428c6cf32a9b83d22fd
Arslan Shakoor <arslantechielive@gmail.com>
2017-10-15 16:50:21 -0400

    auth inital completion and refactoring the code

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const passport = require('express');
-const GoogleStrategy = require('passport-google-oauth20');
+require('./services/passport');
+
 const app = express();
 
-// passport register google strategy
-passport.use(new GoogleStrategy());
+// call the authroutes and pass the app object
+require('./routes/authRoutes')(app);
 
-// app.get('/', (req, res) => {
-//   res.send({ hi: 'there is commit' });
-// });
-// client_id = 540085184818-h7m5q072ro0lnp0gn729ingo7fu2c3pi.apps.googleusercontent.com
-// client_secret = a-lKs1oEHFr6fh5rbgwrgoTV
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
