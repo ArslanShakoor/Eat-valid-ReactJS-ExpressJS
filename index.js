b893750f315b8f76ad17d79b2eead092dd2ebdbc
Arslan Shakoor <arslantechielive@gmail.com>
2017-10-15 13:19:37 -0400

    added new files

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const passport = require('express');
+const GoogleStrategy = require('passport-google-oauth20');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there is commit' });
-});
+// passport register google strategy
+passport.use(new GoogleStrategy());
 
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there is commit' });
+// });
+// client_id = 540085184818-h7m5q072ro0lnp0gn729ingo7fu2c3pi.apps.googleusercontent.com
+// client_secret = a-lKs1oEHFr6fh5rbgwrgoTV
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
