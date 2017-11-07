8f4ae5058a8b6c2e602b5eb6ea7136f6f2541b45
Arslan Shakoor <arslantechielive@gmail.com>
2017-11-07 16:43:51 -0500

    stripe keys rendering

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,13 +6,14 @@ import './index.scss';
 
 class Payments extends Component {
   render() {
+    console.log('environment', process.env.REACT_APP_STRIPE_KEY);
     return (
       <StripeCheckout
         name="Eat Valid"
         description="Donate us to keep you updating"
         amount={500}
         token={token => this.props.handleToken(token)}
-        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+        stripeKey="pk_test_fNLHb6MZE8u1Z6ywwZmBURcZ"
       >
         <button className="donate-us btn-primary">Donate Us</button>
       </StripeCheckout>
